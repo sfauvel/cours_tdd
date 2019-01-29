dda83da2a75cf71436099ca905d0faaa529501b5
sfauvel <sfauvel@gmail.com>
2019-01-29 22:21:29 +0100

    Premier commit du cours

---
+++ generate.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Generate documents
+# --attribute language='fr'
+
+. ~/Documents/projects/asciidoctor/generate_core.sh
+
+generate_pdf_landscape tdd_cours
+generate_revealjs tdd_cours
