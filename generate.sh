e9a71eb5d9b4a6e8efbc35bd0c3222239e5bbec3
sfauvel <sfauvel@gmail.com>
2021-01-21 00:54:58 +0100

    Complement pour le fichier PDF

--- generate.sh
+++ generate.sh
@@ -7,7 +7,8 @@ ASCIIDOC_GENERATE_PATH=$(dirname ~/Documents/projects/asciidoctor)/asciidoctor
 echo $ASCIIDOC_GENERATE_PATH
 . ${ASCIIDOC_GENERATE_PATH}/generate_core.sh
 
-generate_pdf_landscape tdd_cours
+generate_pdf tdd_cours tdd_cours_slide "-a pdf-stylesdir=./themes -a pdf-style=slide"
+#generate_pdf_landscape tdd_cours
 #generate_revealjs tdd_cours
 generate_revealjs_css tdd_cours paloit_reveal.css
 
