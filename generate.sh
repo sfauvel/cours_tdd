f9beddc35f3fe35fdddec6cf12431f22c44e7a3a
sfauvel <sfauvel@gmail.com>
2021-01-21 22:30:06 +0100

    Amélioration de la mise en forme et de la génération

--- generate.sh
+++ generate.sh
@@ -4,14 +4,11 @@
 # --attribute language='fr'
 
 ASCIIDOC_GENERATE_PATH=$(dirname ~/Documents/projects/asciidoctor)/asciidoctor
-echo $ASCIIDOC_GENERATE_PATH
 . ${ASCIIDOC_GENERATE_PATH}/generate_core.sh
 
-generate_pdf tdd_cours tdd_cours_slide "-a pdf-stylesdir=./themes -a pdf-style=slide"
-#generate_pdf_landscape tdd_cours
-#generate_revealjs tdd_cours
-generate_revealjs_css tdd_cours paloit_reveal.css
+generate_pdf tdd_cours tdd_cours_slide "-a pdf-stylesdir=./themes -a pdf-style=tdd"
+generate_pdf tdd_atelier tdd_atelier_slide "-a pdf-stylesdir=./themes -a pdf-style=tdd"
 
+generate_revealjs_css tdd_cours paloit_reveal.css
 cp ./content/themes/css/fullscreen.css ./docs/revealjs/css
 
-#generate_pdf_landscape tdd_atelier
