d16648a0e1509f48a4d2f0303e12f62a56f2a3e4
Your Name <sfauvel@gmail.com>
2022-03-20 00:35:55 +0100

    Création du pdf avec un fond blanc

---
+++ createPdf.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+ROOT_PATH="$(pwd)"
+FILENAME=tdd_cours
+PDF_PATH=${ROOT_PATH}/docs/pdf
+REVEALJS_PATH=${ROOT_PATH}/docs/revealjs
+#SIZE=1368x768
+SIZE=1920x1080
+
+html_for_pdf_with_path="./docs/revealjs/${FILENAME}_reveal_for_pdf.html"
+
+cp ./docs/revealjs/tdd_cours_reveal.html $html_for_pdf_with_path
+ls ./docs/revealjs/
+sed -i "s/<link rel=\"stylesheet\" href=\"reveal.js\/css\/theme\/.*.css\" id=\"theme\">/<link rel=\"stylesheet\" href=\"reveal.js\/css\/theme\/white.css\" id=\"theme\">/g" $html_for_pdf_with_path
+
+mkdir -p ${PDF_PATH}
+#https://github.com/astefanutti/decktape
+docker run --rm -t \
+  -v ${PDF_PATH}:/slides \
+  -v ${REVEALJS_PATH}:/revealjs \
+  astefanutti/decktape\
+   /revealjs/${FILENAME}_reveal_for_pdf.html ${FILENAME}_${SIZE}.pdf \
+   --size ${SIZE}
+
+
+rm $html_for_pdf_with_path
